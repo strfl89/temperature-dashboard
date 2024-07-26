1fca2598f4c80069d440bb5e17c5074b857ded5f
strfl89 <strfl89@my-uc-lab.de>
2024-07-26 23:25:05 +0200

    move blebox function from index.php to function file

---
+++ functions/blebox.php
@@ -0,0 +1,12 @@
+function get_blebox_state($ip)
+{
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, "http://".$ip."/state");
+	curl_setopt($ch, CURLOPT_HTTPGET, true);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$response = curl_exec($ch);
+	curl_close($ch);
+
+	return json_decode($response, true);
+
+}
--- index.php
+++ index.php
@@ -17,18 +17,7 @@ date_default_timezone_set('Europe/Berlin');
 
 require_once('./config/sensors.cfg');
 
-function get_blebox_state($ip)
-{
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, "http://".$ip."/state");
-	curl_setopt($ch, CURLOPT_HTTPGET, true);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	$response = curl_exec($ch);
-	curl_close($ch);
-
-	return json_decode($response, true);
-
-}
+require_once('./functions/blebox.php');
 
 echo "<h2>Temperatur &Uuml;bersicht</h2>";
 
